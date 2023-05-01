71da20bfada1009d237d14b8d3e53206b6abce2a
dogatech <dogatech@dogatech.home>
2023-05-01 01:14:01 -0700

    update to get new deployment working in app-engine

--- main.py
+++ main.py
@@ -3,33 +3,33 @@ from bottle import mako_template as template
 from bottle import static_file
 from google.appengine.api import users
 
-bottle = Bottle()
+app = Bottle()
 
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
 
 
-@bottle.route('/')
-@bottle.route('/a/<name>')
+@app.route('/')
+@app.route('/a/<name>')
 def main(name=''):
   return template('views/main.html', is_admin=('true' if users.is_current_user_admin() else 'false'))
 
-@bottle.route('/components/sar/<filepath:path>')
+@app.route('/components/sar/<filepath:path>')
 def sar(filepath):
   return static_file(filepath, root='components/sar')
 
-@bottle.route('/components/<filepath:path>')
+@app.route('/components/<filepath:path>')
 def components(filepath):
   return static_file(filepath, root='bower_components')
 
-@bottle.route('/add')
-@bottle.route('/edit/<name>')
+@app.route('/add')
+@app.route('/edit/<name>')
 def add(name=''):
   return static_file('add.html', root='views')
 
 
 # Define an handler for 404 errors.
-@bottle.error(404)
+@app.error(404)
 def error_404(error):
   """Return a custom 404 error."""
   return 'Sorry, nothing at this URL.'
